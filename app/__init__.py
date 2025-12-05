078ea630c6c2d310d12f08a033ad2dd069b5c4e8
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-12-05 21:37:39 +0200

    fix: update import paths for routes in __init__.py to use relative imports

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 from app.logging_config import configure_logging
 from config import DevelopmentConfig
-from routes.auth import auth_bp
-from routes.groups import groups_bp
-from routes.users import users_bp
+from .routes.auth import auth_bp
+from .routes.groups import groups_bp
+from .routes.users import users_bp
 
 
 def create_app(config_class=DevelopmentConfig):
